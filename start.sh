67743329a2eb6c96930d384bb9057c9615708251
istvan <kosztik@gmail.com>
2017-10-25 06:44:14 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
 
 
 /etc/init.d/nginx start
-/etc/init.d/php-fpm5 start
 /etc/init.d/cron start
 /etc/init.d/clam-daemon start
 /etc/init.d/clam-freshclam start
 /etc/init.d/postfix start
 /etc/init.d/dovecot start
 /etc/init.d/amavis start
+/etc/init.d/php5-fpm start
 
 
 
